59d660ba0ec954f53ad0e0e7a6a22df4c468c299
Zach Bullough <zach@ordermygear.com>
2022-11-23 21:35:03 -0600

    modify betwixt properties to be per language

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::mem;
 
 use nom::branch::alt;
 use nom::bytes::complete::{tag, take_until, take_until1, take_while, take_while1};
 use nom::character::complete::{alpha1, newline, space0};
-use nom::character::{is_newline, is_space};
+use nom::character::{is_alphanumeric, is_newline, is_space};
 use nom::combinator::{all_consuming, map, opt, peek};
 use nom::error::ParseError;
 use nom::sequence::{delimited, pair, preceded, terminated, tuple};
@@ -15,7 +16,6 @@ pub const BETWIXT_COM_TOKEN: &'static str = "<!--btxt";
 pub const CLOSE_TOKEN: &'static str = "?>";
 pub const CLOSE_COM_TOKEN: &'static str = "-->";
 const FILENAME_PROP: &'static str = "filename";
-const LANG_PROP: &'static str = "lang";
 const TAG_PROP: &'static str = "tag";
 const TANGLE_MODE_PROP: &'static str = "mode";
 
@@ -49,29 +49,66 @@ impl<'a> Default for TangleMode<'a> {
     }
 }
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
+// TODO can we get rid of this Clone?
+struct PropertiesCollection<'a> {
+	global: Properties<'a>,
+	languages: HashMap<&'a[u8], Properties<'a>>
+}
+
+impl<'a> PropertiesCollection<'a> {
+	fn get_code_props(&self, lang: Option<&'a [u8]>) -> Properties<'a> {
+		match lang {
+			None => self.global.clone(),
+			Some(lang) => {
+				match self.languages.get(lang) {
+					None => self.global.clone(),
+					Some(lang_props) => {
+						let mut lang_props = lang_props.clone();
+						lang_props.merge(&self.global);
+						lang_props
+					}
+				}
+			}
+		}
+	}
+	
+	fn update(&mut self, lang: Option<&'a[u8]>, mut props: Properties<'a>) {
+		match lang {
+			Some(lang) => {
+				if self.languages.contains_key(lang) {
+					props.merge(self.languages.get(lang).unwrap());
+				}
+				self.languages.insert(lang, props);
+			},
+			None => {
+				props.merge(&self.global);
+				self.global = props;
+			},
+		}
+	}
+}
+
+#[derive(Default, Clone, Debug)]
 pub struct Properties<'a> {
     pub filename: Option<&'a [u8]>,
-    pub language: Option<&'a [u8]>,
     pub tag: Option<&'a [u8]>,
     pub mode: Option<TangleMode<'a>>,
 }
 
 impl<'a> Properties<'a> {
-    fn merge_parent(&mut self, parent: Properties<'a>) {
-        if self.filename.is_none() {
-            self.filename = parent.filename;
-        }
-        if self.language.is_none() {
-            self.language = parent.language;
+
+	fn merge(&mut self, other: &Properties<'a>) {
+		if self.filename.is_none() {
+            self.filename = other.filename;
         }
         if self.tag.is_none() {
-            self.tag = parent.tag;
+            self.tag = other.tag;
         }
         if self.mode.is_none() {
-            self.mode = parent.mode;
-        }
-    }
+            self.mode = other.mode.clone();
+		}
+	}
 }
 
 pub struct Code<'a> {
@@ -85,11 +122,27 @@ pub struct CodePart<'a> {
     pub lang: Option<&'a [u8]>,
 }
 
-pub struct Section<'a> {
-    pub heading: Option<&'a [u8]>,
+pub struct SectionPart<'a> {
+	pub heading: Option<&'a [u8]>,
     pub level: usize,
+}
+
+pub struct Section<'a> {
+	pub part: SectionPart<'a>,
+	properties: PropertiesCollection<'a>,
     code_block_indexes: Vec<usize>,
     pub children: Vec<Section<'a>>,
+}
+
+impl<'a> Section<'a> {
+	fn new(part: SectionPart<'a>, properties: PropertiesCollection<'a>) -> Self {
+		Section{
+			part,
+			properties,
+			children: Vec::new(),
+			code_block_indexes: Vec::new(),
+		}
+	}
 }
 
 pub struct Document<'a> {
@@ -103,43 +156,46 @@ impl<'a> Document<'a> {
         parsers: &mut MarkdownParsers<P1, P2, P3>,
     ) -> Self
     where
-        P1: Parser<&'a [u8], Properties<'a>, nom::error::Error<&'a [u8]>>,
-        P2: Parser<&'a [u8], Section<'a>, nom::error::Error<&'a [u8]>>,
-        P3: Parser<&'a [u8], CodePart<'a>, nom::error::Error<&'a [u8]>>,
+        P1: PropertiesParser<'a>,
+        P2: SectionParser<'a>,
+        P3: CodeParser<'a>,
     {
         let mut next = scan(contents, true, parsers);
-        let mut properties = Properties{
-			..Default::default()
+        let properties = PropertiesCollection{
+			global: Properties{..Default::default()},
+			languages: HashMap::new(),
 		};
 		let mut blocks = Vec::new();
         let mut section = Section {
-            heading: None,
-            level: 0,
+			part: SectionPart{
+				heading: None,
+				level: 0,
+			},
             code_block_indexes: Vec::new(),
+			properties,
             children: Vec::new(),
         };
         // a given index in the stack is the current parent of that level.
         let mut section_frame: [Option<Section>; 10] =
             [None, None, None, None, None, None, None, None, None, None]; // support 9 + root levels of headings
-		let mut props_frame: [Option<Properties>; 10] =
-			[None, None, None, None, None, None, None, None, None, None];
         while next.is_some() {
             let (input, item) = next.unwrap();
             match item {
                 ScanResult::Section(new) => {
-                    if new.level == section.level {
+                    if new.level == section.part.level {
                         // parent section isn't changing, just the active section is.
-                        section_frame[section.level]
+						let props = section.properties.clone();
+                        section_frame[section.part.level]
                             .as_mut()
                             .unwrap()
                             .children
                             .push(section);
-                        section = new;
-                    } else if new.level < section.level {
+                        section = Section::new(new, props);
+                    } else if new.level < section.part.level {
                         // we're going back to a higher level heading. This means append the section
                         // to the current level's parent. Then find the appropriate parent for the new
                         // level and set the new current parent.
-                        section_frame[section.level]
+                        section_frame[section.part.level]
                             .as_mut()
                             .unwrap()
                             .children
@@ -149,13 +205,10 @@ impl<'a> Document<'a> {
                             for idx in new.level + 1..10 {
                                 if section_frame[idx].is_some() {
                                     section_frame.swap(new.level, idx);
-									properties = props_frame[idx].clone().unwrap();
                                     break;
                                 }
                             }
-                        } else {
-							properties = props_frame[new.level].clone().unwrap();
-						}
+                        }
 						// all children lower (numerically higher) than the new section
 						// will never get a chance to be reconciled. We need to do so now.
 						for idx in (new.level+1..10).rev() {
@@ -163,31 +216,32 @@ impl<'a> Document<'a> {
 								let mut child = None;
 								mem::swap(&mut section_frame[idx], &mut child);
 								let child = child.unwrap();
-								section_frame[child.level].as_mut().unwrap().children.push(child);
+								section_frame[child.part.level].as_mut().unwrap().children.push(child);
 							}
 						}
-                        section = new;
+						let idx = new.level;
+                        section = Section::new(new, section_frame[idx].as_ref().unwrap().properties.clone());
                     } else { // going to a child section
-						props_frame[new.level] = Some(properties.clone());
+						let props = section.properties.clone();
                         section_frame[new.level] = Some(section);
-                        section = new;
+                        section = Section::new(new, props);
                     }
                 }
                 ScanResult::Code(code) => {
 					section.code_block_indexes.push(blocks.len());
+					let props = section.properties.get_code_props(code.lang);
 					blocks.push(Code{
-						properties: properties.clone(),
+						properties: props,
 						part: code,
 					});
 				},
-                ScanResult::Properties(mut props) => {
-                    props.merge_parent(properties);
-                    properties = props;
+                ScanResult::Properties(props) => {
+					section.properties.update(props.0, props.1);
                 }
             }
             next = scan(input, false, parsers);
         }
-        section_frame[section.level]
+        section_frame[section.part.level]
             .as_mut()
             .unwrap()
             .children
@@ -212,25 +266,25 @@ pub struct MarkdownParsers<P1, P2, P3> {
 
 enum ScanResult<'a> {
     Code(CodePart<'a>),
-    Section(Section<'a>),
-    Properties(Properties<'a>),
+    Section(SectionPart<'a>),
+    Properties((Option<&'a[u8]>, Properties<'a>)),
 }
 
 pub fn betwixt<'a>(
     start: &'static str,
     end: &'static str,
-) -> impl FnMut(&'a [u8]) -> IResult<&[u8], Properties> {
+) -> impl FnMut(&'a [u8]) -> IResult<&'a [u8], (Option<&'a[u8]>, Properties)> {
     move |i: &[u8]| {
-        let (input, body) = delimited(tag(start), take_until(end), tag(end))(i)?;
+        let (input, (lang, body)) = delimited(tag(start), pair(opt(preceded(tag("+"), take_while(is_alphanumeric))), take_until(end)), tag(end))(i)?;
         let properties = properties(body)?;
-        Ok((input, properties.1))
+        Ok((input, (lang, properties.1)))
     }
 }
 
-pub fn code(
+pub fn code<'a>(
     code_start: &'static str,
     code_end: &'static str,
-) -> impl Fn(&[u8]) -> IResult<&[u8], CodePart> {
+) -> impl Fn(&'a[u8]) -> IResult<&'a[u8], CodePart> {
     move |i: &[u8]| {
         let (input, (_, lang, _, _)) = tuple((tag(code_start), opt(alpha1), space0, tag("\n")))(i)?;
         let mut terminator = locate_parser_match(tuple((tag(code_end), space0, newline)));
@@ -241,7 +295,7 @@ pub fn code(
             CodePart {
                 contents: &input[..end_idx],
                 lang,
-            },
+			},
         ))
     }
 }
@@ -268,8 +322,8 @@ where
 }
 
 // Parse out a section between header levels
-pub fn section(mark: char) -> impl Fn(&[u8]) -> IResult<&[u8], Section> {
-    move |i: &[u8]| {
+pub fn section<'a>(mark: char) -> impl Fn(&'a[u8]) -> IResult<&'a[u8], SectionPart> {
+    move |i: &'a[u8]| {
         let (input, (header, _, heading)) = tuple((
             take_while1(|c| c == mark as u8),
             take_while1(is_space),
@@ -277,15 +331,20 @@ pub fn section(mark: char) -> impl Fn(&[u8]) -> IResult<&[u8], Section> {
         ))(i)?;
         Ok((
             input,
-            Section {
+            SectionPart {
                 heading: Some(heading),
                 level: header.input_len(),
-                code_block_indexes: Vec::new(),
-                children: Vec::new(),
             },
         ))
     }
 }
+
+pub trait PropertiesParser<'a>: Parser<&'a [u8], (Option<&'a [u8]>, Properties<'a>), nom::error::Error<&'a [u8]>> {}
+impl<'a, T> PropertiesParser<'a> for T where T: Parser<&'a [u8], (Option<&'a [u8]>, Properties<'a>), nom::error::Error<&'a [u8]>> {}
+pub trait SectionParser<'a>: Parser<&'a [u8], SectionPart<'a>, nom::error::Error<&'a [u8]>> {}
+impl<'a, T> SectionParser<'a> for T where T: Parser<&'a [u8], SectionPart<'a>, nom::error::Error<&'a [u8]>> {}
+pub trait CodeParser<'a>: Parser<&'a [u8], CodePart<'a>, nom::error::Error<&'a [u8]>> {}
+impl<'a, T> CodeParser<'a> for T where T: Parser<&'a [u8], CodePart<'a>, nom::error::Error<&'a [u8]>> {}
 
 fn scan<'a, P1, P2, P3>(
     i: &'a [u8],
@@ -293,9 +352,9 @@ fn scan<'a, P1, P2, P3>(
     parsers: &mut MarkdownParsers<P1, P2, P3>,
 ) -> Option<(&'a [u8], ScanResult<'a>)>
 where
-    P1: Parser<&'a [u8], Properties<'a>, nom::error::Error<&'a [u8]>>,
-    P2: Parser<&'a [u8], Section<'a>, nom::error::Error<&'a [u8]>>,
-    P3: Parser<&'a [u8], CodePart<'a>, nom::error::Error<&'a [u8]>>,
+    P1: PropertiesParser<'a>,
+    P2: SectionParser<'a>,
+    P3: CodeParser<'a>,
 {
 	let mut new_line = first;
     for idx in 0..i.input_len() {
@@ -336,8 +395,8 @@ fn property<'a>(t: &'static str) -> impl Fn(&[u8]) -> IResult<&[u8], &[u8]> {
 // all have matched or all remaining fail. Returns None for any unmatches parsers
 // TODO make this a macro cause this is silly.
 fn opt_permutation<P, I, O, E>(
-    mut parsers: (P, P, P, P),
-) -> impl FnMut(I) -> IResult<I, (Option<O>, Option<O>, Option<O>, Option<O>), E>
+    mut parsers: (P, P, P),
+) -> impl FnMut(I) -> IResult<I, (Option<O>, Option<O>, Option<O>), E>
 where
     P: Parser<I, O, E>,
     E: ParseError<I>,
@@ -345,7 +404,7 @@ where
 {
     move |i: I| {
         let mut success = true;
-        let mut results = (None, None, None, None);
+        let mut results = (None, None, None);
         let mut input = i;
         while success {
             success = false;
@@ -370,13 +429,6 @@ where
                     input = i;
                 }
             }
-            if results.3.is_none() {
-                if let Ok((i, output)) = parsers.3.parse(input.clone()) {
-                    results.3 = Some(output);
-                    success = true;
-                    input = i;
-                }
-            }
         }
         Ok((input, results))
     }
@@ -384,16 +436,14 @@ where
 
 fn properties(i: &[u8]) -> IResult<&[u8], Properties> {
     let fname = property(FILENAME_PROP);
-    let lang = property(LANG_PROP);
     let tag = property(TAG_PROP);
     let mode = property(TANGLE_MODE_PROP);
-    let (input, (filename, language, tag, mode)) =
-        all_consuming(opt_permutation((fname, lang, tag, mode)))(i)?;
+    let (input, (filename, tag, mode)) =
+        all_consuming(opt_permutation((fname, tag, mode)))(i)?;
     Ok((
         input,
         Properties {
             filename,
-            language,
             tag,
             mode: match mode {
                 Some(mode) => Some(TangleMode::from_bytes(mode)?.1),
@@ -409,26 +459,26 @@ mod tests {
 
     #[test]
     fn test_betwixt() {
-        let btxt = &b"<?btxt tag='test1'
-lang=\"rust\" mode=\"overwrite\" filename='test/src/lib.rs'   ?>";
+        let btxt = &b"<?btxt+rust tag='test1'
+ mode=\"overwrite\" filename='test/src/lib.rs'   ?>";
         let mut betwixt = betwixt(BETWIXT_TOKEN, CLOSE_TOKEN);
         let res = betwixt(&btxt[..]);
         assert!(res.is_ok(), "valid betwixt body should parse successfully");
         let props = res.unwrap().1;
         assert_eq!(
-            props.tag,
+            props.1.tag,
             Some(&b"test1"[..]),
             "should parse 'tag' successfully"
         );
         assert_eq!(
-            props.filename,
+            props.1.filename,
             Some(&b"test/src/lib.rs"[..]),
             "should parse 'filename' successfully"
         );
-        assert!(props.mode.is_some());
-        assert!(matches!(props.mode.unwrap(), TangleMode::Overwrite));
+        assert!(props.1.mode.is_some());
+        assert!(matches!(props.1.mode.unwrap(), TangleMode::Overwrite));
         assert_eq!(
-            props.language,
+            props.0,
             Some(&b"rust"[..]),
             "should parse 'lang' successfully"
         );
@@ -585,7 +635,7 @@ println!(\"test\");
 ```
 ### Section 3A
 some content that we don't care about
-<?btxt lang='rust' filename='foo.rs'?>
+<?btxt filename='foo.rs'?>
 ```rust
 // some comment goes here
 ```
@@ -604,7 +654,7 @@ Ignore all this fluff";
         let root = doc.root;
         assert_eq!(2, root.children.len());
 		// children[0] Section 2A
-		assert_eq!(Some(&b"Section 2A"[..]), root.children[0].heading);
+		assert_eq!(Some(&b"Section 2A"[..]), root.children[0].part.heading);
         assert_eq!(root.children[0].code_block_indexes.len(), 1);
         assert_eq!(
             doc.code_blocks[root.children[0].code_block_indexes[0]].properties.filename,
@@ -614,18 +664,15 @@ Ignore all this fluff";
             doc.code_blocks[root.children[0].code_block_indexes[0]].part.contents,
             &b"println!(\"test\");\n"[..]
         );
-        assert_eq!(doc.code_blocks[root.children[0].code_block_indexes[0]].properties.language, None);
         assert_eq!(root.children[0].children.len(), 1);
-		assert_eq!(Some(&b"Section 3A"[..]), root.children[0].children[0].heading);
+		assert_eq!(Some(&b"Section 3A"[..]), root.children[0].children[0].part.heading);
 		assert_eq!(root.children[0].children[0].children.len(), 2);
 		assert_eq!(root.children[0].children[0].code_block_indexes.len(), 1);
 		assert_eq!(doc.code_blocks[root.children[0].children[0].code_block_indexes[0]].properties.filename, Some(&b"foo.rs"[..]));
-		assert_eq!(doc.code_blocks[root.children[0].children[0].code_block_indexes[0]].properties.language, Some(&b"rust"[..]));
 
 		// children[1] Section 2B
-		assert_eq!(Some(&b"Section 2B"[..]), root.children[1].heading);
+		assert_eq!(Some(&b"Section 2B"[..]), root.children[1].part.heading);
 		assert_eq!(1, root.children[1].code_block_indexes.len());
-		assert_eq!(None, doc.code_blocks[root.children[1].code_block_indexes[0]].properties.language);
 		assert_eq!(Some(&b"test.rs"[..]), doc.code_blocks[root.children[1].code_block_indexes[0]].properties.filename);
     }
 }
